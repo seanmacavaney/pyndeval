c98723eb35cf4182d31e2412038a6c84a75a94fe
Sean MacAvaney <sean.macavaney@gmail.com>
2024-10-01 15:50:32 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyndeval",
-    version="0.0.3",
+    version="0.0.4",
     author="Sean MacAvaney",
     author_email="sean.macavaney@glasgow.ac.uk",
     description="Interface to ndeval.c",
